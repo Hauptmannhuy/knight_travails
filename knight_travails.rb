21cf3a49ac70587cefe84bb54e7efb5cd75af75c
Hauptmannhuy <dindago2324@gmail.com>
2023-12-11 19:54:46 +0200

    rubocop corrections

--- knight_travails.rb
+++ knight_travails.rb
@@ -1,84 +1,79 @@
 class Board
   attr_accessor :chessboard
+
   def initialize
-    @chessboard = Array.new(8){Array.new(8){Square.new}}
+    @chessboard = Array.new(8) { Array.new(8) { Square.new } }
   end
-  
-  def knight_moves(start,destination)
+
+  def knight_moves(start, destination)
     queue = [start]
     visited = []
     path = []
     until queue.empty?
-  
-     current_cell = queue.shift
-     path << current_cell
-     return reconstruct_path(path,destination) if current_cell == destination
-     
-     self.chessboard.each_with_index do |row,i|
-      row.each_with_index do |cell,j|
-        if cell == current_cell && !visited.include?(cell)
-          moves = adjacent_squares(i,j)
-          moves.select!{|move| !visited.include?(move)}
-          moves.each{|move| move.pointer = current_cell}
+
+      current_cell = queue.shift
+      path << current_cell
+      return reconstruct_path(path, destination) if current_cell == destination
+
+      chessboard.each_with_index do |row, i|
+        row.each_with_index do |cell, j|
+          next unless cell == current_cell && !visited.include?(cell)
+
+          moves = adjacent_squares(i, j)
+          moves.select! { |move| !visited.include?(move) }
+          moves.each { |move| move.pointer = current_cell }
           queue.concat(moves)
         end
       end
+      visited << current_cell
     end
-    visited << current_cell
+    puts 'No path found.'
   end
-  puts 'No path found.'
-    end
-  
-  
-  def adjacent_squares(i,j)
+
+  def adjacent_squares(i, j)
     moves = []
     possible_moves = [
       [i + 2, j + 1], [i + 2, j - 1], [i - 2, j + 1], [i - 2, j - 1],
       [i - 1, j + 2], [i - 1, j - 2], [i + 1, j - 2], [i + 1, j + 2]
     ]
     possible_moves.each do |move|
-      x,y = move
-      moves << valid_move?(x,y)
+      x, y = move
+      moves << valid_move?(x, y)
     end
     moves.compact
   end
-  
-  def valid_move?(x,y)
-    if x.between?(0,7) && y.between?(0,7) 
-      self.chessboard[x][y].coord = "[#{x},#{y}]"
-     self.chessboard[x][y]
-    else
-      nil
-    end
+
+  def valid_move?(x, y)
+    return unless x.between?(0, 7) && y.between?(0, 7)
+
+    chessboard[x][y].coord = "[#{x},#{y}]"
+    chessboard[x][y]
   end
-  
-  def reconstruct_path(path,destination)
-    from_destination = path.find{|square| square == destination}
+
+  def reconstruct_path(path, destination)
+    from_destination = path.find { |square| square == destination }
     distance = 0
     coordinates = []
     while from_destination
       coordinates << from_destination.coord
       from_destination = from_destination.pointer
-      distance+=1
+      distance += 1
     end
     puts "You made it in #{distance} moves!  Here's your path: "
-    coordinates.reverse.each{|coord| puts coord}
+    coordinates.reverse.each { |coord| puts coord }
   end
-  
-  end
-  
-  class Square
+end
+
+class Square
   attr_accessor :pointer, :coord
+
   def initialize
     @pointer = nil
     @coord = nil
-    
   end
-  end
-  
-  board = Board.new
-  start = board.chessboard[0][0]
-  destination = board.chessboard[7][7]
-  board.knight_moves(start,destination)
-  
-  
+end
+
+board = Board.new
+start = board.chessboard[0][0]
+destination = board.chessboard[7][7]
+board.knight_moves(start, destination)
