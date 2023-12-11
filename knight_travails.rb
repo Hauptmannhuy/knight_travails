8eacaa346314322b37db639ee83708e3eb9780d9
Hauptmannhuy <dindago2324@gmail.com>
2023-12-11 19:41:32 +0200

    implemented reconstruct path

--- knight_travails.rb
+++ knight_travails.rb
@@ -3,29 +3,32 @@ class Board
   def initialize
     @chessboard = Array.new(8){Array.new(8){Square.new}}
   end
-
+  
   def knight_moves(start,destination)
     queue = [start]
     visited = []
+    path = []
     until queue.empty?
-
+  
      current_cell = queue.shift
-     return current_cell if current_cell == destination
+     path << current_cell
+     return reconstruct_path(path,destination) if current_cell == destination
      
      self.chessboard.each_with_index do |row,i|
       row.each_with_index do |cell,j|
         if cell == current_cell && !visited.include?(cell)
-          puts "[#{i},#{j}]"
           moves = adjacent_squares(i,j)
           moves.select!{|move| !visited.include?(move)}
+          moves.each{|move| move.pointer = current_cell}
           queue.concat(moves)
         end
       end
     end
     visited << current_cell
   end
+  puts 'No path found.'
     end
-
+  
   
   def adjacent_squares(i,j)
     moves = []
@@ -39,18 +42,43 @@ class Board
     end
     moves.compact
   end
-
+  
   def valid_move?(x,y)
-    x.between?(0,7) && y.between?(0,7) ? self.chessboard[x][y] : nil
+    if x.between?(0,7) && y.between?(0,7) 
+      self.chessboard[x][y].coord = "[#{x},#{y}]"
+     self.chessboard[x][y]
+    else
+      nil
+    end
   end
-
-end
-
-class Square
-
-end
-
-board = Board.new
-start = board.chessboard[3][3]
-destination = board.chessboard[3][3]
-board.knight_moves(start,destination)
+  
+  def reconstruct_path(path,destination)
+    from_destination = path.find{|square| square == destination}
+    distance = 0
+    coordinates = []
+    while from_destination
+      coordinates << from_destination.coord
+      from_destination = from_destination.pointer
+      distance+=1
+    end
+    puts "You made it in #{distance} moves!  Here's your path: "
+    coordinates.reverse.each{|coord| puts coord}
+  end
+  
+  end
+  
+  class Square
+  attr_accessor :pointer, :coord
+  def initialize
+    @pointer = nil
+    @coord = nil
+    
+  end
+  end
+  
+  board = Board.new
+  start = board.chessboard[0][0]
+  destination = board.chessboard[7][7]
+  board.knight_moves(start,destination)
+  
+  
